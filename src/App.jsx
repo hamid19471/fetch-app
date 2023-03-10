c116ef5e69643dcca3f27c4a4808461c8d842fec
hamid19471 <hamid.19471@gmail.com>
2023-03-10 14:06:43 +0330

    Upate: import mainApp component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import "./App.css";
-
+import MainApp from "./Components/MainApp/MainApp";
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="flex flex-col items-center justify-center w-full mx-auto my-10">
+            <MainApp />
+        </div>
+    );
 }
 
 export default App;
